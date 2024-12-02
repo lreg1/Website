fd0b7078ea1449af97b75a8ac3a20f7948cefc7b
lreg1 <leoareg@yahoo.com>
2024-12-02 14:28:29 -0500

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,15 @@
+var elem = document.getElementById("thing")
+var orig = elem.outerHTML
+var words = elem.innerHTML
+window.addEventListener('scroll', function() {if (window.scrollY >= elem.parentElement.scrollHeight + 180) {change()} else {unChange()}} );
+function change() {
+  elem.outerHTML = "<ul id='stuff'>" + words + "</ul>"
+  elem = document.getElementById("stuff")
+  console.log("Said hi");
+}
+function unChange() {
+  elem.outerHTML = orig
+  elem = document.getElementById("thing")
+  // elem.outerHTML = "<p id='thing'></p>"
+  console.log("Unsaid hi");
+}
